47477cb9cebac73a4c3af37539c5f8525cda1808
Quimbayo Trujillo Andres Felipe <afquimbayo.ext@rbm.com.co>
2025-06-18 11:41:41 -0500

    Se suben cambios version pago exitoso

--- src/test/java/TestAPIS/TestGetPayment.java
+++ src/test/java/TestAPIS/TestGetPayment.java
@@ -5,6 +5,9 @@ import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import Utils.FileStore;
+
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 
 @Feature("Api Testing")
@@ -17,13 +20,14 @@ public class TestGetPayment {
         BaseTest config = new BaseTest();
         config.setUp();
         Allure.getLifecycle().updateTestCase(testResult -> testResult.setName("Verificar consulta de pago exitosamente"));
-        int bankId = 51;
+        int bankId = 888;
         String endpoint = "/cert/api/paymentInitiation/" + bankId + "/" + domesticPayment;
+        String uuid = UUID.randomUUID().toString();
         Response response = given()
                 .filter(new AllureRestAssured())
                 .header("Host", "vj4mk6pz0m.execute-api.us-east-1.amazonaws.com")
                 .header("Content-Type", "application/json")
-                .header("x-fapi-interaction-id", "e82bc935-644f-4c7e-9273-86d41059cef7")
+                .header("x-fapi-interaction-id", uuid)
                 .when()
                 .get(endpoint);
         response.then().log().all().assertThat().statusCode(200);
--- src/test/java/TestAPIS/TestPostPaymentInitiation.java
+++ src/test/java/TestAPIS/TestPostPaymentInitiation.java
@@ -8,6 +8,10 @@ import Utils.FileStore;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
 import static io.restassured.RestAssured.given;
 @Feature("API Testing")
 public class TestPostPaymentInitiation {
@@ -17,9 +21,15 @@ public class TestPostPaymentInitiation {
         BaseTest config = new BaseTest();
         config.setUp();
         Allure.getLifecycle().updateTestCase(testResult -> testResult.setName("Verificar un pago exitosamente"));
-        int bankId = 51;
+        int bankId = 888;
         String endpoint = "cert/api/paymentInitiation/" + bankId;
-        String jsonBody = new String(Files.readAllBytes(Paths.get("src/test/resources/json/postpayment.json")));
+        String jsonBodyTemplate = new String(Files.readAllBytes(Paths.get("src/test/resources/json/postpayment.json")));
+        String uuid = UUID.randomUUID().toString();
+        double randomAmount = ThreadLocalRandom.current().nextDouble(1000.00, 20000.00);
+        String amountFormatted = String.format(Locale.US, "%.2f", randomAmount);
+        String jsonBody = jsonBodyTemplate
+                .replace("{{$randomUUID}}", uuid)
+                .replace("{{$randomAmount}}", amountFormatted);
         Response response = given()
                 .filter(new AllureRestAssured())
                 .header("x-fapi-interaction-id", "e82bc935-644f-4c7e-9273-86d41059cef7")
